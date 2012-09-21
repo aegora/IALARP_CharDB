cd731e2d054af8a138627896ea1ca95b1aa28076
Chris G <palinuri@msn.com>
2012-09-20 18:58:19 -0600

    Frest Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IALARPChardb::Application.config.secret_token = 'd56122a547d899748ba9efad2971741af27ebcb198ef5f304df8565d209b2ee9ef969d3ecb940ee4aeed59505830dfce0c2e9e4a487d27a0facd9d67b27161cd'
